aa13dbc703fb37fd0cf41ab96168206d29fb05cf
merinpriyasha <merinpriyasha@gmail.com>
2026-02-02 09:48:53 +0530

    impliment user, product and comment routes

--- backend/src/db/queries.ts
+++ backend/src/db/queries.ts
@@ -1,5 +1,5 @@
 import { db } from "./index";
-import { eq, ne } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import {
   users,
   comments,
@@ -9,8 +9,7 @@ import {
   type NewProduct,
 } from "./schema";
 
-//USER QUERIES
-
+// USER QUERIES
 export const createUser = async (data: NewUser) => {
   const [user] = await db.insert(users).values(data).returning();
   return user;
@@ -34,48 +33,49 @@ export const updateUser = async (id: string, data: Partial<NewUser>) => {
   return user;
 };
 
-//upsert => create or update
+// upsert => create or update
 
 export const upsertUser = async (data: NewUser) => {
-  //this is what we have done first
-  //const existingUser = await getUserById(data.id);
-  //if (existingUser) return updateUser(data.id, data);
+  // this is what we have done first
+  // const existingUser = await getUserById(data.id);
+  // if (existingUser) return updateUser(data.id, data);
 
-  //return createUser(data);
+  // return createUser(data);
 
-  //and this is what CR suggested
+  // and this is what CR suggested
   const [user] = await db
     .insert(users)
     .values(data)
-    .onConflictDoUpdate({ target: users.id, set: data })
+    .onConflictDoUpdate({
+      target: users.id,
+      set: data,
+    })
     .returning();
-
   return user;
 };
 
-//PRODUCT QUESRIES
-
-const createProduct = async (data: NewProduct) => {
+// PRODUCT QUERIES
+export const createProduct = async (data: NewProduct) => {
   const [product] = await db.insert(products).values(data).returning();
   return product;
 };
 
-export const getAllProduct = async () => {
+export const getAllProducts = async () => {
   return db.query.products.findMany({
     with: { user: true },
-    orderBy: (products, { desc }) => [desc(products.createdAt)], //desc means: you will see the latest product
+    orderBy: (products, { desc }) => [desc(products.createdAt)], // desc means: you will see the latest products first
     // the square brackets are required because Drizzle ORM's orderBy expects an array, even for a single column.
   });
 };
 
-export const getProductById = async(id: string) => {
+export const getProductById = async (id: string) => {
   return db.query.products.findFirst({
     where: eq(products.id, id),
     with: {
       user: true,
       comments: {
-        with: {user: true},
-        orderBy: (comments,{ desc }) => [desc(comments.createdAt)],
+        with: { user: true },
+        orderBy: (comments, { desc }) => [desc(comments.createdAt)],
       },
     },
   });
@@ -84,18 +84,22 @@ export const getProductById = async(id: string) => {
 export const getProductsByUserId = async (userId: string) => {
   return db.query.products.findMany({
     where: eq(products.userId, userId),
-    with: {user: true},
-    orderBy: (products, {desc}) => [desc(products.createdAt)],
+    with: { user: true },
+    orderBy: (products, { desc }) => [desc(products.createdAt)],
   });
 };
 
 export const updateProduct = async (id: string, data: Partial<NewProduct>) => {
   const existingProduct = await getProductById(id);
-  if(!existingProduct) {
+  if (!existingProduct) {
     throw new Error(`Product with id ${id} not found`);
   }
 
-  const [product] = await db.update(products).set(data).where(eq(products.id, id)).returning();
+  const [product] = await db
+    .update(products)
+    .set(data)
+    .where(eq(products.id, id))
+    .returning();
   return product;
 };
 
@@ -105,8 +109,35 @@ export const deleteProduct = async (id: string) => {
     throw new Error(`Product with id ${id} not found`);
   }
 
-  const [product] = await db.delete(products).where(eq(products.id, id)).returning();
+  const [product] = await db
+    .delete(products)
+    .where(eq(products.id, id))
+    .returning();
   return product;
-
 };
 
+// COMMENT QUERIES
+export const createComment = async (data: NewComment) => {
+  const [comment] = await db.insert(comments).values(data).returning();
+  return comment;
+};
+
+export const deleteComment = async (id: string) => {
+  const existingComment = await getCommentById(id);
+  if (!existingComment) {
+    throw new Error(`Comment with id ${id} not found`);
+  }
+
+  const [comment] = await db
+    .delete(comments)
+    .where(eq(comments.id, id))
+    .returning();
+  return comment;
+};
+
+export const getCommentById = async (id: string) => {
+  return db.query.comments.findFirst({
+    where: eq(comments.id, id),
+    with: { user: true },
+  });
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,32 @@
 import express from "express";
 import { ENV } from "./config/env";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
 
 const app = express();
 
 app.use(clerkMiddleware());
-app.use(cors({origin:ENV.FRONTEND_URL}))
+app.use(cors({ origin: ENV.FRONTEND_URL }));
 app.get("/", (req, res) => {
   const {} = req.body;
-    res.json({
-    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+  res.json({
+    message:
+      "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
     endpoints: {
       users: "/api/users",
       products: "/api/products",
       comments: "/api/comments",
     },
   });
-})
-app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT))
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on PORT:", ENV.PORT),
+);
